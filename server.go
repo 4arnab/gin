d4ffd2e4b509902a86a4bb1d3704fe90b309a076
ahmed <axmedarna3@gmail.com>
2023-05-22 05:22:19 +0300

    getting data from the body

--- server.go
+++ server.go
@@ -15,14 +15,28 @@ func main() {
 		})
 	})
 
-	server.GET("/products/", func(context *gin.Context) {
+	type USER struct {
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+	}
+
+	newUser := USER{
+		Name: "Ahmed Arnab",
+		Age:  19,
+	}
+	server.POST("/products/", func(context *gin.Context) {
 		page := context.Query("page")
 		size := context.Query("size")
 
 		// formData := context.PostForm()
+		// jsonData, _ := ioutil.ReadAll(context.Request.Body)
 
-		fmt.Print()
-		context.JSON(200, map[string]string{"page": page, "size": size})
+		var user USER
+		context.BindJSON(&user)
+
+		fmt.Print(user)
+		// context.JSON(200, map[string]string{"page": page, "size": size})
+		context.JSON(200, gin.H{"user": newUser, "page": page, "size": size, "bodyData": user})
 	})
 
 	fmt.Println("server listening on port 4000")
